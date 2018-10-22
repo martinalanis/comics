89816c0869b9d052839d10221204982617648730
martinalanis <martinalanis.dev@gmail.com>
2018-10-22 17:51:25 -0500

    Header, slider y books terminado

---
+++ js/main.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+
+	// $('#slider-home').slick({
+ //        infinite: true,
+ //        slidesToShow: 1,
+ //        slidesToScroll: 1,
+ //        swipeToSlide: true,
+ //        autoplay: true,
+ //        autoplaySpeed: 3000,
+ //        arrows: true,
+ //        dots: false,
+ //    });
+
+});
