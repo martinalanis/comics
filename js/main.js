bf2f600811deeb4f70817d8d4feffdcd80979cd2
martinalanis <martinalanis.dev@gmail.com>
2018-10-22 20:24:03 -0500

    Terminado home

--- js/main.js
+++ js/main.js
@@ -1,14 +1,4 @@
 $(document).ready(function(){
 
-	// $('#slider-home').slick({
- //        infinite: true,
- //        slidesToShow: 1,
- //        slidesToScroll: 1,
- //        swipeToSlide: true,
- //        autoplay: true,
- //        autoplaySpeed: 3000,
- //        arrows: true,
- //        dots: false,
- //    });
 
 });
